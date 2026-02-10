541a64ec695e80812a509159809743341ad98937
cesarFuenmayor <cesaralexff2019@gmail.com>
2026-02-10 11:38:03 -0400

    no hice nada

--- database/migrations/2026_01_18_000000_create_users_table.php
+++ database/migrations/2026_01_18_000000_create_users_table.php
@@ -32,9 +32,7 @@ return new class extends Migration
             )->nullable();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
-            $table->foreignId('roleId')->constrained(
-                table:'roles', indexName:'users_roles_id'
-            );
+            
             $table->string('photoUser')->nullable();
             $table->boolean('status')->default(1);
             $table->rememberToken();
