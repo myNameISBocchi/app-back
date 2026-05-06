6ca26a9bc7aadecd849888326acaef05e3db7f27
MyNameISBocchi <espinaalfonso123@gmail.com>
2026-05-06 19:27:39 -0400

    routes

--- routes/api.php
+++ routes/api.php
@@ -16,42 +16,31 @@ use App\Http\Controllers\stateController;
 
 
 Route::post('/loggin', [AuthController::class, 'loggin']);
- 
+
 
 Route::middleware('auth')->group(function () {
 
    
-    Route::middleware('auth:ADMINISTRADOR')->group(function () {
-        
-        
-        Route::post('/roles', [roleController::class, 'store']);
-        Route::put('/roles/{id}', [roleController::class, 'update']);
-        Route::delete('roles/{id}', [roleController::class, 'delete']);
-        Route::post('/privileges', [privilegesController::class, 'store']);
-        Route::put('privileges/{id}', [privilegesController::class, 'update']);
-        Route::delete('privileges/{id}', [privilegesController::class, 'delete']);
-        Route::post('/rolesPrivileges', [rolePrivilegeController::class, 'store']);
+    Route::get('/comunities', [ComunityController::class, 'findAll']);
+    Route::get('/councils', [CouncilController::class, 'findAll']);
+    Route::get('/committees', [CommitteeController::class, 'findAll']); 
+    Route::get('/roles', [roleController::class, 'findAll']);
+    Route::get('/privileges', [privilegesController::class, 'findAll']);
+    Route::get('/cities', [citieController::class, 'findAll']);
+    Route::get('/countries', [countryController::class, 'findAll']);
+    Route::get('/states', [stateController::class, 'findAll']);
+    
+  
+    Route::post('/peoples', [personController::class, 'store']);
+    Route::get('/peoples/search', [personController::class, 'searchPerson']);
+    Route::get('/peoples/{id}', [personController::class, 'findById']);
+    Route::put('/peoples/{id}', [personController::class, 'update']);
+    Route::post('/peoples/{id}/photo', [personController::class, 'uploadPhoto']);
+    Route::get('rolesPrivileges/{roleId}', [rolePrivilegeController::class, 'findPrivilegeByRoleId']);
 
-        
-        Route::get('/peoples', [personController::class, 'findAll']);
-        Route::delete('/peoples/{id}', [personController::class, 'delete']);
-
-        
-        Route::post('/countries', [countryController::class, 'store']);
-        Route::put('countries/{id}', [countryController::class, 'update']);
-        Route::delete('/countries/{id}', [countryController::class, 'delete']);
-        Route::post('/states', [stateController::class, 'store']);
-        Route::put('/states/{id}', [stateController::class, 'update']);
-        Route::delete('/states/{id}', [stateController::class, 'delete']);
-        Route::post('/cities', [citieController::class, 'store']);
-        Route::put('/cities/{id}', [citieController::class, 'update']);
-        Route::delete('/cities/{id}', [citieController::class, 'delete']);
-        
-    });
 
     Route::middleware('auth:ADMINISTRADOR,LIDER DE COMUNA')->group(function () {
-        
-        
+        // Gestión de Comunidades
         Route::post('/comunities', [ComunityController::class, 'store']);
         Route::put('/comunities/{id}', [ComunityController::class, 'update']);
         Route::delete('/comunities/{id}', [ComunityController::class, 'delete']);
@@ -61,26 +50,35 @@ Route::middleware('auth')->group(function () {
         Route::post('/councils', [CouncilController::class, 'store']);
         Route::put('/councils/{id}', [CouncilController::class, 'update']);
         Route::delete('/councils/{id}', [CouncilController::class, 'delete']);
-        Route::post('/committes', [CommitteeController::class, 'store']);
+
+      
+        Route::post('/committees', [CommitteeController::class, 'store']); 
     });
 
-  
-    Route::get('/comunities', [ComunityController::class, 'findAll']);
-    Route::get('/councils', [CouncilController::class, 'findAll']);
-    Route::get('/committes', [CommitteeController::class, 'findAll']);
-    Route::get('/roles', [roleController::class, 'findAll']);
-    Route::get('/privileges', [privilegesController::class, 'findAll']);
-    Route::get('rolesPrivileges/{roleId}', [rolePrivilegeController::class, 'findPrivilegeByRoleId']);
     
-    Route::get('/peoples/search', [personController::class, 'searchPerson']);
-    Route::post('/peoples', [personController::class, 'store']);
-    Route::get('/peoples/{id}', [personController::class, 'findById']);
-    Route::put('/peoples/{id}', [personController::class, 'update']);
-    Route::post('/peoples/{id}/photo', [personController::class, 'uploadPhoto']);
+    Route::middleware('auth:ADMINISTRADOR')->group(function () {
+        Route::post('/roles', [roleController::class, 'store']);
+        Route::put('/roles/{id}', [roleController::class, 'update']);
+        Route::delete('roles/{id}', [roleController::class, 'delete']);
+        
+        Route::post('/privileges', [privilegesController::class, 'store']);
+        Route::put('privileges/{id}', [privilegesController::class, 'update']);
+        Route::delete('privileges/{id}', [privilegesController::class, 'delete']);
+        
+        Route::post('/rolesPrivileges', [rolePrivilegeController::class, 'store']);
+        
+        Route::get('/peoples', [personController::class, 'findAll']);
+        Route::delete('/peoples/{id}', [personController::class, 'delete']);
 
-   
-    Route::get('/countries', [countryController::class, 'findAll']);
-    Route::get('/countries/{country}', [countryController::class, 'findById']);
-    Route::get('/states', [stateController::class, 'findAll']);
-    Route::get('/cities', [citieController::class, 'findALl']);
+        // Ubicaciones Geográficas
+        Route::post('/countries', [countryController::class, 'store']);
+        Route::put('countries/{id}', [countryController::class, 'update']);
+        Route::delete('/countries/{id}', [countryController::class, 'delete']);
+        Route::post('/states', [stateController::class, 'store']);
+        Route::put('/states/{id}', [stateController::class, 'update']);
+        Route::delete('/states/{id}', [stateController::class, 'delete']);
+        Route::post('/cities', [citieController::class, 'store']);
+        Route::put('/cities/{id}', [citieController::class, 'update']);
+        Route::delete('/cities/{id}', [citieController::class, 'delete']);
+    });
 });
